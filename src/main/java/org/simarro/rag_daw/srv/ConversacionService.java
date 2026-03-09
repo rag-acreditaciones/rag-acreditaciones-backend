2f6a46a5e51e086ea55197f0fa4e8ce08a829a12
LucasLopezLopez <lulolosmx@gmail.com>
2026-03-09 11:05:34 +0100

    feat: Service de conversacion con CRUD completo

---
+++ src/main/java/org/simarro/rag_daw/srv/ConversacionService.java
@@ -0,0 +1,25 @@
+package org.simarro.rag_daw.srv;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.lang.NonNull;
+
+import org.simarro.rag_daw.model.dto.ConversacionCreateDTO;
+import org.simarro.rag_daw.model.dto.ConversacionDetailDTO;
+import org.simarro.rag_daw.model.dto.ConversacionResponseDTO;
+import org.simarro.rag_daw.model.enums.EstadoConversacion;
+
+public interface ConversacionService {
+
+    ConversacionDetailDTO crearConversacion(@NonNull ConversacionCreateDTO dto);
+
+    Page<ConversacionResponseDTO> listarConversaciones(@NonNull Long usuarioId, String seccionTematica, EstadoConversacion estado,Pageable pageable);
+
+    Optional<ConversacionDetailDTO> obtenerConversacion(@NonNull Long conversacionId);
+
+    Optional<ConversacionDetailDTO> archivarConversacion(@NonNull Long conversacionId,@NonNull Long usuarioId);
+
+    boolean eliminarConversacion(@NonNull Long conversacionId, @NonNull Long usuarioId);
+}
