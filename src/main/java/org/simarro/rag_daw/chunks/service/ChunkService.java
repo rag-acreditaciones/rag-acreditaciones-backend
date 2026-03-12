70564f2f55c097a5593c36e1ce21011abae34abf
JorgePR91 <georgos14@gmail.com>
2026-03-12 22:23:20 +0100

    Finalitzat el Service i mètodes de recerca per id i estadístiques de chunks

---
+++ src/main/java/org/simarro/rag_daw/chunks/service/ChunkService.java
@@ -0,0 +1,23 @@
+package org.simarro.rag_daw.chunks.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.simarro.rag_daw.chunks.model.DTO.ChunkBusquedaDTO;
+import org.simarro.rag_daw.chunks.model.DTO.ChunkDetailDTO;
+import org.simarro.rag_daw.chunks.model.DTO.ChunkResponseDTO;
+import org.simarro.rag_daw.chunks.model.DTO.ChunkStatsDTO;
+import org.simarro.rag_daw.exception.FiltroException;
+import org.simarro.rag_daw.exception.ResourceNotFoundException;
+import org.simarro.rag_daw.model.dto.PaginaResponse;
+
+public interface ChunkService {
+    
+    ChunkDetailDTO findById(Long id);
+    PaginaResponse<ChunkDetailDTO> findByDocumentoId (Long docId, String estado, int page, int size, String[] sort)  throws FiltroException; 
+    PaginaResponse<ChunkBusquedaDTO> buscarPorTexto (String texto, Long seccionId, int page, int size) throws FiltroException;
+    List<ChunkBusquedaDTO> busquedaSemantica (Map<String, Object> body) throws ResourceNotFoundException;
+    ChunkStatsDTO getChunkStats(Long docId);
+    ChunkResponseDTO cambiarEstado(Long id, String nuevoEstado);
+
+}
